2427dc86a288a586824d61a91c8d9a1d6fecab70
gitcommander78 <rawappeal@hotmail.com>
2025-05-03 21:27:53 +0200

    structure code

--- Main.java
+++ Main.java
@@ -1,201 +1,248 @@
 package com.mygdx.game;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class Main extends ApplicationAdapter {
-	// Sprite batch textured planes 
-    private SpriteBatch batch;
-    
-    // Player & Camera 
-    private Texture image;
+
+    SpriteBatch batch;
+    Texture playerTexture;
+    Texture platformTexture;
+    Texture enemyTexture;
+
     Player player;
-    Camera camera;
-    
-    // Enemies
-    Texture imageEnemy;
-    ArrayList<Enemy> enemies;
-    
-    // Game over
-    boolean gameOver = false;
-    BitmapFont font;
-    
-    // Platforms 
-    Texture imagePlatform001;
     ArrayList<Platform> platforms;
+    ArrayList<Enemy> enemies;
+
+    TiledMap map;
+    OrthogonalTiledMapRenderer mapRenderer;
+    OrthographicCamera camera;
+
+    BitmapFont font;
+    boolean gameOver = false;
 
     @Override
     public void create() {
-    	// Initialize new Sprite Batch 
         batch = new SpriteBatch();
-        
-        // Create platforms
-        imagePlatform001 = new Texture("platform001.png");
+        playerTexture = new Texture("mega.png");
+        platformTexture = new Texture("platform001.png");
+        enemyTexture = new Texture("dog.png");
+        map = new TmxMapLoader().load("test.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
         platforms = new ArrayList<>();
-        
-        // Add platforms 
-        platforms.add(new Platform(400, 150, 200, 20, imagePlatform001));
-        platforms.add(new Platform(700, 250, 200, 20, imagePlatform001));
-        platforms.add(new Platform(1000, 350, 200, 20, imagePlatform001));
-        
-        // Add & Create moving platforms 
-        Platform movingPlatform001 = new Platform(500, 200, 200, 20, imagePlatform001);
-        Platform movingPlatform002 = new Platform(700, 50, 100, 50, imagePlatform001);
-        movingPlatform001.setMoving(100, 150); 
-        movingPlatform002.setMoving(200, 250);
-        platforms.add(movingPlatform001);
-        platforms.add(movingPlatform002);
-        
-        // Create player & Camera 
-        image = new Texture("mega.png");
-        player = new Player(image, new Vector2(100, 100), platforms);
-        camera = new Camera(800, 480, 2000, 800);
-        
-        // Create enemies
-        imageEnemy = new Texture("dog.png");
+        float playerSpawnX = 100;
+        float playerSpawnY = 100;
+
+        MapObjects objects = map.getLayers().get("Collisions").getObjects();
+        for (MapObject object : objects) {
+            if (object instanceof RectangleMapObject) {
+                RectangleMapObject rectObject = (RectangleMapObject) object;
+
+                float x = rectObject.getRectangle().x;
+                float y = rectObject.getRectangle().y;
+                float width = rectObject.getRectangle().width;
+                float height = rectObject.getRectangle().height;
+
+                String type = rectObject.getProperties().get("type", String.class);
+
+                if ("platform".equals(type)) {
+                    platforms.add(new Platform(x, y, width, height, platformTexture));
+                }
+
+                if ("player_spawn".equals(type)) {
+                    playerSpawnX = x;
+                    playerSpawnY = y;
+                }
+            }
+        }
+
+        platforms.add(new Platform(400, 150, 200, 20, platformTexture));
+        platforms.add(new Platform(700, 250, 200, 20, platformTexture));
+        platforms.add(new Platform(1000, 350, 200, 20, platformTexture));
+
+        Platform movingPlatform = new Platform(500, 200, 200, 20, platformTexture);
+        movingPlatform.setMoving(100, 150);
+        platforms.add(movingPlatform);
+
+        player = new Player(playerTexture, new Vector2(playerSpawnX, playerSpawnY), platforms);
+
         enemies = new ArrayList<>();
-        
-        // Add moving enemies 
-        Enemy e = new Enemy(imageEnemy, new Vector2(600, 300), 550, 650);
-        e.setMoving(60, 100, false); // Horizontal moving enemy
+
+        Enemy e = new Enemy(enemyTexture, new Vector2(600, 300), 600, 700);
+        e.setMoving(60, 100, false);
         enemies.add(e);
-        
-        // Add still enemies 
-        enemies.add(new Enemy(imageEnemy, new Vector2(500, 200), 500, 200));
-        enemies.add(new Enemy(imageEnemy, new Vector2(600, 100), 600, 100));
-        
-        // Add game over
+
+        Enemy f = new Enemy(enemyTexture, new Vector2(800, 200), 200, 400);
+        f.setMoving(50, 200, true);
+        enemies.add(f);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
         font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(2);
+    }
+
+    public void update(float delta) {
+
+        if (gameOver) {
+            if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
+                restartGame();
+            }
+            return;
+        }
+
+        handleInput();
+        player.update(delta);
+
+        float targetX = player.getX() + player.getWidth() / 2;
+        float targetY = player.getY() + player.getHeight() / 2;
+        camera.position.x += (targetX - camera.position.x) * 0.1f;
+        camera.position.y += (targetY - camera.position.y) * 0.1f;
+
+        float levelWidth = 2000;
+        float levelHeight = 800;
+
+        float halfW = camera.viewportWidth / 2;
+        float halfH = camera.viewportHeight / 2;
+
+        camera.position.x = Math.max(halfW, Math.min(camera.position.x, levelWidth - halfW));
+        camera.position.y = Math.max(halfH, Math.min(camera.position.y, levelHeight - halfH));
+
+        camera.update();
+
+        for (Enemy e : enemies) {
+            if (e.isActive()) {
+                e.update(delta);
+
+                for (EnemyBullet b : e.bullets) {
+                    if (b.isActive() && player.getBounds().contains(b.position)) {
+                        b.active = false;
+                        player.takeDamage();
+                        if (!player.playerAlive) gameOver = true;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        for (Enemy e : enemies) {
+            if (!e.isActive()) continue;
+
+            for (int i = player.bullets.size() - 1; i >= 0; i--) {
+                Bullet b = player.bullets.get(i);
+
+                if (e.getBounds().overlaps(new Rectangle(b.position.x, b.position.y, 8, 8))) {
+                    e.hit();
+                    b.active = false;
+                    player.bullets.remove(i);
+                    break;
+                }
+            }
+        }
+
+        for (Platform p : platforms) {
+            p.update(delta);
+        }
+    }
+
+    public void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            player.moveLeft();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            player.moveRight();
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            player.jump();
+        }
+        
+        float shootX = 0;
+        float shootY = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            shootX = -1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            shootX = 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            shootY = 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            shootY = -1;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
+            player.shoot(shootX, shootY);
+        }
+    }
+
+    public void restartGame() {
+        player = new Player(playerTexture, new Vector2(100, 100), platforms);
+        gameOver = false;
     }
 
     @Override
     public void render() {
-    	// Update screen 
-    	update(Gdx.graphics.getDeltaTime());
-    	
-    	Gdx.gl.glClearColor(0, 0, 0, 1);
-    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    	
-    	batch.setProjectionMatrix(camera.getCamera().combined);
-    	batch.begin();
-    	
-    	// Draw player
-    	if (!gameOver) {
-    	    player.draw(batch);
-    	}
-    	
-    	// Draw enemies
-    	for (Enemy e: enemies) {
-    		if (e.isActive()) {
-    			e.draw(batch);
-    		}
-    	}
-    	
-    	// Draw platforms
-    	for (Platform p : platforms) {
-    	    p.draw(batch);
-    	}
-    	
-    	// Draw game over
-    	if (gameOver) {
-    		font.draw(batch,  "GAME OVER - Press R to restart", camera.getCamera().position.x - 100, camera.getCamera().position.y);
-    	}
+        update(Gdx.graphics.getDeltaTime());
 
-        batch.end();
-    }
-    
-    public void update(float delta) {
-    	// Update game over
-    	if (gameOver) {
-    		if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
-    			restartGame();
-    		}
-    		return;
-    	}
-    	
-    	// Update input & player 
-    	handleInput();
-    	player.update(delta);
-    	
-    	EnemyManager.playerPosition = new Vector2(player.getX(), player.getY());
-    	
-    	// Update camera 
-    	camera.update(player.getX() + player.getWidth() / 2, player.getY() + player.getHeight() / 2);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    	// Update enemies 
-    	for (Enemy e: enemies) {
-    		if (e.isActive()) {
-    			e.update(delta);
-    		}
-    	}
-    	
-    	// Update player shooting enemies & collision 
-    	for (Enemy e: enemies) {
-    		if (!e.isActive()) continue;
-    		
-    		for (Bullet b: player.bullets) {
-    			if (b.isActive() && e.getBounds().contains(b.position)) {
-    				e.hit();
-    				b.active = false;
-    				break;
-    			}
-    		}
-    	}
-    	
-    	// Update enemy disposing player & collision 
-    	for (Enemy e : enemies) {
-            if (!e.isActive()) continue;
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
 
-            for (EnemyBullet b : e.bullets) {
-                if (b.isActive() && player.getBounds().contains(b.position)) {
-                    b.active = false;
-                    player.takeDamage();
-                    if (!player.playerAlive) {
-                        gameOver = true;
-                    }
-                    break; 
-                }
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        for (Platform p : platforms) {
+            p.draw(batch);
+        }
+
+        if (!gameOver) {
+            player.draw(batch);
+        }
+
+        for (Enemy e : enemies) {
+            if (e.isActive()) {
+                e.draw(batch);
             }
         }
-    	
-    	// Update platforms
-    	for (Platform p: platforms) {
-    		p.update(delta);
-    	}
-    }
-    
-    private void handleInput() {
-    	if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-    		Gdx.app.exit();
-    	}
-    }
-    
-    public void restartGame() {
-    	// Reset player
-    	player = new Player(image, new Vector2(100, 100), platforms);
-    	
-    	// Reset enemies
-    	enemies.clear();
-        enemies.add(new Enemy(imageEnemy, new Vector2(500, 200), 500, 200));
-        enemies.add(new Enemy(imageEnemy, new Vector2(600, 100), 600, 100));
-        
-        // Reset game over
-        gameOver  = false;
-    	
+
+        if (gameOver) {
+            font.draw(batch, "GAME OVER - Press R to Restart", camera.position.x - 200, camera.position.y);
+        }
+
+        batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        playerTexture.dispose();
+        platformTexture.dispose();
+        enemyTexture.dispose();
+        map.dispose();
+        mapRenderer.dispose();
         font.dispose();
     }
 }
--- Player.java
+++ Player.java
@@ -1,151 +1,161 @@
 package com.mygdx.game;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class Player {
-	// Player logic
-	Texture texture;
-	Vector2 position;
-	float speed = 300f;
-	float velocityY = 0f;
-	int tripleJump = 3;
-	
-	// Health logic 
-	int playerHealth = 5;
-	public boolean playerAlive = true;
-	
-	// Shooting logic
-	ArrayList<Bullet> bullets = new ArrayList<>();
-	Texture imageBullet = new Texture("shoot01.png");
-	
-	// Platform logic
-	ArrayList<Platform> platforms;
-	
-	// Constructor player
-	public Player(Texture texture, Vector2 startPosition, ArrayList<Platform> platforms) {
-		this.texture = texture;
-		this.position = startPosition;
-		this.platforms = platforms;
-	}
-	
-	public Rectangle getBounds() {
-		return new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
-	}
-	
-	public void takeDamage() {
-		playerHealth--;
-		if (playerHealth <= 0) {
-			playerAlive = false;
-		}
-	}
-	
-	public void update(float delta) {
-		handleInput(delta);
-		
-		// Gravity
-		velocityY -= 500 * delta;
-		position.y += velocityY * delta;
-		
-		// Ground collision
-		boolean onPlatform = false;
 
-		for (Platform p : platforms) {
-		    if (position.y > p.getBounds().y + p.getBounds().height - 1) {
-		        if (position.x + texture.getWidth() > p.getBounds().x &&
-		            position.x < p.getBounds().x + p.getBounds().width) {
+    Vector2 position;
+    Texture texture;
 
-		            if (position.y + velocityY * delta <= p.getBounds().y + p.getBounds().height) {
-		                position.y = p.getBounds().y + p.getBounds().height;
-		                velocityY = 0;
-		                tripleJump = 3;
-		                onPlatform = true;
-		                if (p.moving) {
-		                    position.x += p.moveSpeed * p.direction * delta;
-		                }
-		                break;
-		            }
-		        }
-		    }
-		}
+    float velocityY = 0;
+    final float GRAVITY = -600;
 
-		// Ground collision if not on platform 
-		if (!onPlatform && position.y <= 0) {
-		    position.y = 0;
-		    velocityY = 0;
-		    tripleJump = 3;
-		}
-		
-		// Update bullets
-		for (int i = bullets.size() - 1; i >= 0; i--) {
-			Bullet b = bullets.get(i);
-			b.update(delta);
-			if (!b.isActive()) {
-				bullets.remove(i);
-			}
-		}
-	}
-	
-	public void handleInput(float delta) {
-		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-			position.x -= speed * delta;
-		}
-		
-		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-			position.x += speed * delta;
-		}
-		
-		if (Gdx.input.isKeyJustPressed(Input.Keys.W) && tripleJump > 0) {
-			velocityY = 300;
-			tripleJump--;
-		}
-		
-		// Shooting logic 
-		int shootX = 0;
-		int shootY = 0;
-		
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) shootX = -1;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) shootX = 1;
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) shootY = 1;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) shootY = -1;
-		
-		if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
-			Vector2 shootDir = new Vector2(shootX, shootY);
-			if (shootDir.isZero()) {
-				shootDir.x = 1;
-			}
-			bullets.add(new Bullet(imageBullet, new Vector2(position.x + texture.getWidth() / 2, position.y + texture.getHeight() / 2), shootDir));
-		}
-	}
-	
-	public void draw(SpriteBatch batch) {
-		batch.draw(texture, position.x, position.y);
-		
-		// Draw bullets
-		for (Bullet b: bullets) {
-			b.draw(batch);
-		}
-	}
-	
-	public float getX() {
+    ArrayList<Bullet> bullets = new ArrayList<>();
+
+    ArrayList<Platform> platforms;
+
+    int tripleJump = 3;
+
+    int playerHealth = 5;
+    boolean playerAlive = true;
+
+    public Player(Texture texture, Vector2 startPosition, ArrayList<Platform> platforms) {
+        this.texture = texture;
+        this.position = startPosition;
+        this.platforms = platforms;
+
+        // Make sure player starts alive + full health
+        this.playerHealth = 5;
+        this.playerAlive = true;
+    }
+
+    public void update(float delta) {
+        if (!playerAlive) {
+            return;
+        }
+
+        velocityY += GRAVITY * delta;
+        position.y += velocityY * delta;
+
+        // Check for platform collisions
+        boolean onPlatform = false;
+
+        for (Platform p : platforms) {
+            if (velocityY <= 0) {
+                if (position.y >= p.getBounds().y + p.getBounds().height - 5) {
+                    if (position.x + texture.getWidth() > p.getBounds().x &&
+                            position.x < p.getBounds().x + p.getBounds().width) {
+
+                        if (position.y + velocityY * delta <= p.getBounds().y + p.getBounds().height) {
+                            // Land on platform
+                            position.y = p.getBounds().y + p.getBounds().height;
+                            velocityY = 0;
+                            tripleJump = 3;
+                            onPlatform = true;
+
+                            // Move with platform if moving
+                            if (p.moving) {
+                                position.x += p.moveSpeed * p.direction * delta;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Ground check
+        if (!onPlatform && position.y <= 0) {
+            position.y = 0;
+            velocityY = 0;
+            tripleJump = 3;
+        }
+
+        // Update bullets
+        for (int i = bullets.size() - 1; i >= 0; i--) {
+            Bullet b = bullets.get(i);
+            b.update(delta);
+            if (!b.isActive()) {
+                bullets.remove(i);
+            }
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, position.x, position.y);
+
+        for (Bullet b : bullets) {
+            b.draw(batch);
+        }
+    }
+
+    public void moveLeft() {
+        if (!playerAlive) return;
+
+        position.x -= 200 * Gdx.graphics.getDeltaTime();
+    }
+
+    public void moveRight() {
+        if (!playerAlive) return;
+
+        position.x += 200 * Gdx.graphics.getDeltaTime();
+    }
+
+    public void jump() {
+        if (!playerAlive) return;
+
+        if (tripleJump > 0) {
+            velocityY = 400;
+            tripleJump--;
+        }
+    }
+
+    public void shoot(float shootX, float shootY) {
+        if (!playerAlive) return;
+
+        Vector2 shootDir = new Vector2(shootX, shootY);
+        if (shootDir.isZero()) {
+            shootDir = new Vector2(1, 0); // Default to right
+        } else {
+            shootDir.nor();
+        }
+
+        bullets.add(new Bullet(new Texture("shoot01.png"),
+                new Vector2(position.x + texture.getWidth() / 2, position.y + texture.getHeight() / 2),
+                shootDir));
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
+    }
+
+    public float getX() {
         return position.x;
     }
-    
+
     public float getY() {
         return position.y;
     }
-    
+
     public float getWidth() {
         return texture.getWidth();
     }
-    
+
     public float getHeight() {
-    	return texture.getHeight();
+        return texture.getHeight();
+    }
+
+    public void takeDamage() {
+        playerHealth--;
+
+        if (playerHealth <= 0) {
+            playerAlive = false;
+        }
     }
 }
